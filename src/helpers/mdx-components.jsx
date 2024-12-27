a2107ddc042659ad93b78c9f16f5cb396f2e251f
Fer <ferp@protonmail.ch>
2024-12-27 23:28:47 +0100

    CircularColorsDemo working as expected

--- src/helpers/mdx-components.jsx
+++ src/helpers/mdx-components.jsx
@@ -4,4 +4,5 @@ import dynamic from "next/dynamic";
 export const COMPONENT_MAP = {
   pre: CodeSnippet,
   DivisionGroupsDemo: dynamic(() => import("@/components/DivisionGroupsDemo")),
+  CircularColorsDemo: dynamic(() => import("@/components/CircularColorsDemo")),
 }
